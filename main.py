dd78ffad30717f0d1b1290efba18b52ed53815fa
magisterblaman <hannes.blaman@lbs.se>
2024-09-17 10:11:44 +0200

    Konvertering

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
-my_var = "heusj" # string
-x = 43 # int
-y = 13.37 # float
-z = 0
+import math
 
-sum = x + y
-diff = x - z
-product = x * y
-quotient = y / z
+x = 4.999
 
+x_but_int = int(x)
+x_but_rounded = round(x) # avrunda
+x_but_rounded_down = math.floor(x) # avrunda alltid neråt
+x_but_rounded_up = math.ceil(x) # avrunda alltid uppåt
 
-print(diff)
+print(x_but_rounded)
+
+y = 43
+y_but_float = float(y)
+print(y_but_float)
+
+a = input("Ge mig ett värde på a! ")
+b = input("Ge mig ett värde på b! ")
+
+a_but_int = int(a)
+b_but_int = int(b)
+
+sum = a_but_int + b_but_int
+
+print(sum)
